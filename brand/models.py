4a80f19b9164118ddc51e8dede47907db56e9f83
siner308 <siner308@gmail.com>
2021-07-25 15:49:47 +0900

    당장 필요없는 데이터 입력하지 않도록 수정

--- brand/models.py
+++ brand/models.py
@@ -10,14 +10,14 @@ from user.models import User
 class Brand(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4)
     name = models.CharField(max_length=150)
-    description = models.TextField(null=True)
-    hashtags = models.ManyToManyField(Hashtag)
-    created_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='create_brands',
+    description = models.TextField(blank=True, null=True)
+    hashtags = models.ManyToManyField(Hashtag, blank=True)
+    created_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL, related_name='create_brands',
                                    related_query_name='create_brand')
-    updated_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='update_brands',
+    updated_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL, related_name='update_brands',
                                    related_query_name='update_brand')
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, blank=True, null=True)
 
     objects = models.Manager()
 
--- place/models.py
+++ place/models.py
@@ -14,12 +14,12 @@ class Place(models.Model):
     latitude = models.FloatField()
     longitude = models.FloatField()
     is_deleted = models.BooleanField(default=False)
-    description = models.TextField()
+    description = models.TextField(blank=True, null=True)
     hashtags = models.ManyToManyField(Hashtag)
     brand = models.ForeignKey(Brand, null=True, on_delete=models.SET_NULL)
-    created_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='create_places',
+    created_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL, related_name='create_places',
                                    related_query_name='create_place')
-    updated_by = models.ForeignKey(User, null=True, on_delete=models.SET_NULL, related_name='update_places',
+    updated_by = models.ForeignKey(User, blank=True, null=True, on_delete=models.SET_NULL, related_name='update_places',
                                    related_query_name='update_place')
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
