1ab218bc6aaec64cb7c817faf6c8792629c1fe37
siner308 <siner308@gmail.com>
2021-10-04 01:41:11 +0900

    brand hashtag 추가

--- src/apps/brand/serializers.py
+++ src/apps/brand/serializers.py
@@ -1,9 +1,11 @@
 from rest_framework import serializers
-
 from src.apps.brand.models import Brand
+from src.apps.hashtag.serializers import HashtagResponseSerializer
 
 
 class BrandResponseSerializer(serializers.ModelSerializer):
+    hashtags = HashtagResponseSerializer(many=True, read_only=True)
+
     class Meta:
         model = Brand
         fields = '__all__'
