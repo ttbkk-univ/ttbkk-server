4265de177ee2f577fb88e26e669f20d4b9d85499
siner308 <siner308@gmail.com>
2021-11-28 18:09:47 +0900

    admin display 추가 및 쿼리 최적화

--- src/apps/hashtag/admin.py
+++ src/apps/hashtag/admin.py
@@ -3,4 +3,10 @@ from django.contrib import admin
 # Register your models here.
 from src.apps.hashtag.models import Hashtag
 
+
+class HashtagAdmin(admin.ModelAdmin):
+    list_display = ['name', 'created_at']
+    search_fields = ['name']
+
+
 admin.site.register(Hashtag)
