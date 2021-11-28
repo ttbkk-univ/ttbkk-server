9e369b1c745503f33eeb3ce7c0c71554a9ed1375
siner308 <siner308@gmail.com>
2021-11-28 18:28:22 +0900

    brand를 place_count desc로 정렬되도록 수정

--- src/apps/brand/admin.py
+++ src/apps/brand/admin.py
@@ -1,12 +1,22 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib.admin import display
+from django.db.models import Count
+
 from src.apps.brand.models import Brand
 
 
 class BrandAdmin(admin.ModelAdmin):
-    list_display = ['id', 'name', 'created_at', 'updated_at']
+    list_display = ['id', 'name', 'get_place_count', 'created_at', 'updated_at']
     search_fields = ['name']
+
+    def get_queryset(self, request):
+        return Brand.objects.annotate(place_count=Count('place')).order_by('-place_count').all()
+
+    @display(description='place_count')
+    def get_place_count(self, obj):
+        return obj.place_count
 
 
 admin.site.register(Brand, BrandAdmin)
